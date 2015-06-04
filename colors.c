a93add0aa2691f15be2444ed2069b7f45762a365
sin <sin@2f30.org>
2015-06-04 19:38:28 +0100

    Style fix

--- colors.c
+++ colors.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include "arg.h"
 #include "colors.h"
 #include "queue.h"
--- png.c
+++ png.c
@@ -2,6 +2,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <png.h>
 #include "colors.h"
 
